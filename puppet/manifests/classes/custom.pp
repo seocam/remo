3dd2ff5d57f11b5a88c6c98d6b6d0a68e7e56913
Sergio Oliveira <sergio@tracy.com.br>
2014-05-13 07:54:16 -0300

    Fixes setlocale warnings

--- puppet/manifests/classes/custom.pp
+++ puppet/manifests/classes/custom.pp
@@ -1,3 +1,7 @@
 # You can add custom puppet manifests for your app here.
 class custom {
+  exec { 'append-lc_ctype-to-environments':
+    command => 'echo "LC_CTYPE=en_US.UTF-8" >> /etc/environment',
+    unless  => 'grep LC_CTYPE="en_US.UTF-8" /etc/environment',
+  }
 }
